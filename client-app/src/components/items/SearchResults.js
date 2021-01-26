f5c5cfa32118ad1461c5179ffb112a5afc9ea824
niamiteva <mitevania@gmail.com>
2021-01-26 12:27:40 +0200

    edit search result style + add search over the results

--- client-app/src/components/items/SearchResults.js
+++ client-app/src/components/items/SearchResults.js
@@ -4,10 +4,16 @@ import LoadingSpinner from 'react-loader-spinner'
 import { makeStyles, Theme } from '@material-ui/core/styles';
 import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
-import {Button, Grid, Typography, Card, CardActionArea, CardContent, CardMedia, Hidden } from '@material-ui/core';
+import {Card, CardActionArea, CardContent, CardMedia, Hidden } from '@material-ui/core';
+import { Grid, Typography, Button, Divider, IconButton, InputBase, Paper } from '@material-ui/core';
+import {Link} from 'react-router-dom';
 import {search} from '../../api/search';
 
 const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '80%',
+    padding: '5%'
+  },
   card: {
     display: 'flex',
     minWidth: '100%',
@@ -34,14 +40,39 @@ const useStyles = makeStyles((theme) => ({
     width: '90%',
     background: '#B0BEC5',
     color: '#fff'
-  }
+  },
+  search: {
+    position: 'relative',
+    width: '100%',
+    padding: '2px 4px',
+      display: 'flex',
+      alignItems: 'center',
+  },
+  input: {   
+    marginLeft: theme.spacing(1),  
+    flex: 1, 
+  },
+  iconButton: {
+    padding: 10,
+  },
+  divider: {
+    height: 28,
+    margin: 4,
+  },
 }));
 
 export default function SearchResult(props) {
   const classes = useStyles();
   const [searchResult, setResult] = useState({});
   const [isLoading, setLoading] = useState(true);
+  const [values, setValues] = useState({
+    searchText: '',
+  });
   const searchText = queryString.parse(props.location.search);
+
+  const handleChange = (name) => (event) => {
+    setValues({ ...values, [name]: event.target.value });
+  };
 
   useEffect(() => {
     const abortController = new AbortController();
@@ -76,11 +107,31 @@ export default function SearchResult(props) {
 
     return (
       <main>
-        <Grid container direction="column" justify="center" alignItems="center">
+        <Grid container className={classes.root}>
+        <Grid item md={12} maxWidth="md">
+            <Paper component="form" className={classes.search}>
+              <IconButton className={classes.iconButton} aria-label="menu">
+                <MenuIcon />
+              </IconButton>
+              <Divider className={classes.divider} orientation="vertical" />
+              <InputBase
+                className={classes.input}
+                placeholder="Търсене..."
+                inputProps={{ 'aria-label': 'търсене...' }}
+                id="search" type="search"
+                value={values.searchText} onChange={handleChange('searchText')} 
+              />
+              <Link to={"/search/remedy?q=" + values.searchText }>
+                <IconButton className={classes.iconButton} aria-label="searchText">
+                  <SearchIcon />
+                </IconButton>     
+              </Link>
+            </Paper>
+          </Grid> 
           {isLoading && (<LoadingSpinner />)}
-          {!isLoading && searchResult.map(item => (
-
-          <Grid item md={6} spacing={3}> 
+          <Grid container  direction="row" justify="center" alignItems="center">
+          {!isLoading && searchResult.map(item => (      
+          <Grid item md={6} spacing={3} maxWidth="md"> 
             <CardActionArea component="a" href={item.itemUrl}>
               <Card className={classes.card} spacing={3}> 
                 <Grid container spacing={3}>
@@ -117,8 +168,9 @@ export default function SearchResult(props) {
               </Card>
             </CardActionArea>
           </Grid>
-
+          
           ))}
+        </Grid>
         </Grid>
       </main> 
     );
