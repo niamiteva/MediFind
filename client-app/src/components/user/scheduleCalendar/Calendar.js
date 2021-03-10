9383c31071d02b42502748115b30f53b390423b7
niamiteva <mitevania@gmail.com>
2021-03-10 21:18:22 +0200

    different calendar  view

--- client-app/src/components/user/scheduleCalendar/Calendar.js
+++ client-app/src/components/user/scheduleCalendar/Calendar.js
@@ -5,28 +5,30 @@ import MenuItem from "@material-ui/core/MenuItem";
 import { ViewState } from "@devexpress/dx-react-scheduler";
 import { withStyles } from "@material-ui/core/styles";
 import {
+  ViewSwitcher,
+  DayView,
+  MonthView,
+} from "@devexpress/dx-react-scheduler-material-ui";
+import {
   Scheduler,
   WeekView,
   Toolbar,
   DateNavigator,
   Appointments,
-  AllDayPanel
+  AllDayPanel,
 } from "@devexpress/dx-react-scheduler-material-ui";
-
-
 
 const styles = (theme) => ({
   container: {
     display: "flex",
     marginBottom: theme.spacing(2),
-    justifyContent: "flex-end"
+    justifyContent: "flex-end",
   },
   text: {
     ...theme.typography.h6,
-    marginRight: theme.spacing(2)
-  }
+    marginRight: theme.spacing(2),
+  },
 });
-
 
 export default class Demo extends React.PureComponent {
   constructor(props) {
@@ -35,7 +37,7 @@ export default class Demo extends React.PureComponent {
     this.state = {
       //data: appointments,
       currentDate: "2018-06-27",
-      locale: "bg-BG"
+      locale: "bg-BG",
     };
 
     this.changeLocale = (event) =>
@@ -48,13 +50,19 @@ export default class Demo extends React.PureComponent {
     return (
       <div>
         <Paper>
-          <Scheduler data={data} locale={locale} height={660}>
-            <ViewState defaultCurrentDate={currentDate} />
+          <Scheduler data={data} locale={locale} height={635}>
+            <ViewState
+              defaultCurrentDate={currentDate}
+              defaultCurrentViewName="Week"
+            />
             <WeekView startDayHour={9} endDayHour={19} />
+            <DayView />
+            <MonthView />
             <Toolbar />
             <DateNavigator />
-            <Appointments />
             <AllDayPanel />
+            <ViewSwitcher />
+            <Appointments />
           </Scheduler>
         </Paper>
       </div>
